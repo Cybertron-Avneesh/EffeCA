1882e4d00de892afee57b86c328b25dc48bfc0e2
southpole01 <karankunwar59@gmail.com>
2020-05-21 14:40:43 +0530

    Contacts Page Is Ready

--- lib/screens/contact.dart
+++ lib/screens/contact.dart
@@ -1,10 +1,27 @@
 import 'dart:convert';
-
 import 'package:EffeCA/Utils/constants.dart';
 import 'package:EffeCA/Utils/shared_preference_helper.dart';
 import 'package:EffeCA/components/navDrawer.dart';
 import 'package:EffeCA/model/user.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> _makePhoneCall(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+Future<void> _sendEmail(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 class ContactScreen extends StatefulWidget {
   static const String id = 'contact_screen';
@@ -42,7 +59,128 @@ class _ContactScreenState extends State<ContactScreen> {
           // Add Logout Feature
         ],
       ),
-      body: Center(child: Text('This is Contacts Screen')),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height:5,
+          ),
+          Center(
+            child: RaisedButton(
+              child: Text(
+                "Email: effervescence@iiita.ac.in",
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black87,
+                  fontSize: 15,
+                ),
+              ),
+              onPressed: ()=>_sendEmail("mailto:effervescence@iiita.ac.in"),
+              elevation: 1,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height:10,
+          ),
+          Center(
+            child: Text(
+              "App Related Issue",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height:20,
+          ),
+          ContactDrawer(name: "Ritik Hirchani",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Avneesh Kumar",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Karan Kunwar",phoneNo: "7908175902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Ananya",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Tejas",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Text(
+              "Any Other Issue",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height:20,
+          ),
+          ContactDrawer(name: "Ritik Hirchani",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Avneesh Kumar",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Karan Kunwar",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Ananya",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Tejas",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Avneesh Kumar",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Karan Kunwar",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+          ContactDrawer(name: "Ananya",phoneNo: "7908195902",email: "karankunwar59@gmail.com"),
+        ],
+      ),
     );
+  }
+}
+
+class ContactDrawer extends StatefulWidget {
+  ContactDrawer({this.name,this.phoneNo,this.email});
+  final name;
+  final phoneNo;
+  final email;
+  @override
+  _ContactDrawerState createState() => _ContactDrawerState();
+}
+
+class _ContactDrawerState extends State<ContactDrawer> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 40),
+          child: Text(
+            widget.name,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 40),
+          child: Row(
+            children:<Widget>[
+              IconButton(
+                splashColor: Colors.lightBlueAccent,
+                icon: Icon(
+                  EvaIcons.email,
+                  color: Colors.red,
+                ),
+                onPressed: (){
+                  _sendEmail('mailto:${widget.email}');
+                },
+              ),
+              IconButton(
+                splashColor: Colors.lightBlueAccent,
+                icon: Icon(
+                  EvaIcons.phone,
+                  color: Colors.blue,
+                ),
+                onPressed: (){
+                  _makePhoneCall('tel:${widget.phoneNo}');
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );;
   }
 }
